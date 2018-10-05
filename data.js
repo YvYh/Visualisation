cc2186bc6b1262476b99c206af57da0064fd0a68
Hong <hong.yu@groupe-esigelec.org>
2018-10-05 11:45:28 +0200

    try amChart with ParisData API

--- data.js
+++ data.js
@@ -46,6 +46,7 @@ for (var i=0; i<nbSeries;i++){
     colour.push(`hsla(${teinte},40%,90%,50%)`);
 }
 
+
 //colonnes
 for (i = 1; i < nbColonnes+1; i++) {
     var lineX = disColonne*i;
@@ -58,14 +59,21 @@ for (i = 1; i < nbColonnes+1; i++) {
 var disEtiquette = disColonne/3-marge;
 for (i = 0; i<nbSeries;i++){
     paper.rect(disEtiquette, lineStart+data.series[i].values[0]*lineLength, 100,30,5).attr({"fill":colour[i]});
-    paper.text(disEtiquette+50, lineStart+data.series[i].values[0]*lineLength+15,data.series[i].name).attr({"font-size": "12px"})
+    var etiquette = paper.text(disEtiquette+50, lineStart+data.series[i].values[0]*lineLength+15,data.series[i].name).attr({"font-size": "12px"})
+    etiquette.click(selection)
     var pointAvant = `${disEtiquette+100},${lineStart+data.series[i].values[0]*lineLength+15}`;
     for (var j =0; j<nbColonnes;j++){
         X = disColonne*(j+1);
-        paper.circle(X,lineStart+data.series[i].values[j]*lineLength,5).attr({fill:colour[i]})
-        var point = `${X},${lineStart+data.series[i].values[j]*lineLength}`;
-        paper.path('M'+pointAvant+'L'+point).attr({"stroke-width":"2px","stroke":colour[i]});
-        pointAvant = point;
+        var point = paper.circle(X,lineStart+data.series[i].values[j]*lineLength,5).attr({fill:colour[i]})
+        //point.clickclick(selection)
+        var pointThis = `${X},${lineStart+data.series[i].values[j]*lineLength}`;
+        var line = paper.path('M'+pointAvant+'L'+pointThis).attr({"stroke-width":"2px","stroke":colour[i]});
+        pointAvant = pointThis;
     }
     
+}
+
+var colourSelected = "#ed000f";
+function selection(e){
+    e.attr({fill:colourSelected});
 }
